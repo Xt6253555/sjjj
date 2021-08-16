d3073d48e5439a9152b57c1d5682d234d162c6a0
txu3 <txu3@trip.com>
2021-08-16 17:01:58 +0800

    默认

--- src/LeetCode/demo1.java
+++ src/LeetCode/demo1.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 public class demo1 {
     public static void main(String[] args) {
-        int num[] = {2,3,4};
-        int[] ints = twoSum1(num, 6);
-        System.out.println(Arrays.toString(ints));
+        str("a");
+        str("a","b");
+        str("a","b","c");
     }
     public static int[] twoSum(int[] nums, int target) {
         int res[] = new int[2];
@@ -36,5 +36,8 @@ public class demo1 {
             map.put(b,i);
         }
         return arr;
+    }
+    public static void str(String... a){
+        System.out.println(Arrays.toString(a));
     }
 }
