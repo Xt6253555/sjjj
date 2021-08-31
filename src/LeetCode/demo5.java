758d10fd2256905864e6cc4ae9b98bdfb87f7ac7
txu3 <txu3@trip.com>
2021-08-31 13:48:42 +0800

    默认

--- src/LeetCode/demo5.java
+++ src/LeetCode/demo5.java
@@ -5,52 +5,19 @@ import java.util.Arrays;
 //最长回文子串
 public class demo5 {
     public static void main(String[] args) {
-        String ababaabc = longestPalindrome1("cabbaf");
+        String ababaabc = longestPalindrome("cabaf");
         System.out.println(ababaabc);
     }
     //中心匹配
-    public static String longestPalindrome(String s) {
-        if(s.length()==0){
-            return "";
-        }
-        int length = s.length();
-        int len =1;
-        int maxLen = 0;
-        int maxStart = 0;
+    public static String longestPalindrome(String s){
+        if(s.length()<2||s==null)return s;
+        int len = 0;
+        int start = 0;
+        int end = 0;
         for (int i = 0; i < s.length(); i++) {
-            int l = i-1;
-            int r = i+1;
-            while (l>=0&&s.charAt(l)==s.charAt(i)){
-                len++;
-                l--;
-            }
-            while (r<length&&s.charAt(r)==s.charAt(i)){
-                len++;
-                r++;
-            }
-            while (l>=0&&r<length&&s.charAt(l)==s.charAt(r)){
-                len=len+2;
-                l--;
-                r++;
-            }
-            if(len>maxLen){
-                maxLen=len;
-                maxStart=l;
-            }
-            len=1;
-        }
-        return s.substring(maxStart+1,maxStart+1+maxLen);
-    }
-    public static String longestPalindrome1(String s){
-        if(s==null)return s;
-        int start =0;
-        int end =0;
-        for (int i = 0; i < s.length(); i++) {
-            //一个点扩散
-            int len1 = expandAroundCenter(s, i, i);
-            //两个点扩散
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1,len2);
+            int l = longest(s, i, i);
+            int r = longest(s, i, i+1);
+            len = Math.max(l,r);
             if(len>end-start){
                 start = i-(len-1)/2;
                 end = i+len/2;
@@ -58,11 +25,11 @@ public class demo5 {
         }
         return s.substring(start,end+1);
     }
-    public static int expandAroundCenter(String s, int left, int right) {
-        while (left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
-            left--;
-            right++;
+    public static int longest(String s,int l,int r){
+        while (l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
         }
-        return right-left-1;
+        return r-l-1;
     }
 }
--- src/LeetCode/demo6.java
+++ src/LeetCode/demo6.java
@@ -8,27 +8,23 @@ public class demo6 {
         String s = convert("PAYPALI", 3);
         System.out.println(s);
     }
-    public static String convert(String s, int numRows) {
-        if(numRows<2){
-            return s;
-        }
-        //把字符串分成numRows份
+    public static String convert(String s, int numRows){
+        if(numRows<2)return s;
         ArrayList<StringBuilder> list = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
             list.add(new StringBuilder());
         }
-        //赋值给它相应的字符串
-        int i =0,flag=-1;
-        for (int j = 0; j < s.length(); j++) {
-            list.get(i).append(s.charAt(j));
-            if(i==0||i==numRows-1){
-                flag=-flag;
+        int flag = -1;
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            list.get(num).append(s.charAt(i));
+            if(num==0||num==numRows-1){
+                flag = -flag;
             }
-            i+=flag;
+            num+=flag;
         }
-        //最后进行拼接
         StringBuilder builder = new StringBuilder();
-        for (StringBuilder stringBuilder :list) {
+        for (StringBuilder stringBuilder : list) {
             builder.append(stringBuilder);
         }
         return builder.toString();
